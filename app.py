971d68a3f632867dd64575059de14aff06ed8473
Majorabe <16669891+majorabe@users.noreply.github.com>
2024-10-25 10:15:32 -0300

    Agregados en la estructura e interfaz de usuario

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 import streamlit as st
 import requests
 
-st.title("API FastAPI con Streamlit")
+icono_url = "uvas.png"  
+
+# Configurar layout con dos columnas para el titulo
+col1, col2 = st.columns([4,0.5])
+with col1:
+    st.title("Predicción de Rendimiento Vitivinícola")
+with col2:
+    st.image(icono_url, width=75)
+
+st.write(f"Tipo de Uva: Malbec")
+st.write(f"Región: Mendoza - Argentina - (Considerando sus 18 Dptos)")
+st.write(f"Periodo de recoleccion de datos: 2013 a 2023")
+
+# Agregar select con opciones
+opciones = ["2024", "2025", "2026", "2027", "2028"]
+
+st.markdown("""
+    <style>
+    div[data-baseweb="select"] > div {
+        width: 200px !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+seleccion = st.selectbox("Año a Pronosticar:", opciones)
+
+st.write(f"Has seleccionado: {seleccion}")
+
 
 # Solicitar un ítem al usuario
 item_id = st.number_input("Ingrese el ID del ítem:", min_value=0, value=1)
@@ -14,3 +41,4 @@ if st.button("Obtener ítem"):
         st.success(f"Ítem obtenido: {data}")
     else:
         st.error("Error al obtener el ítem.")
+        
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+# Importamos FastAPI del módulo fastapi
 from fastapi import FastAPI
 
+# Creamos una instancia de la aplicación FastAPI
 app = FastAPI()
 
+# Definimos una ruta para la raíz ("/") usando el método HTTP GET
 @app.get("/")
 def read_root():
+    # Esta función se ejecuta cuando se accede a la ruta raíz
+    # Retorna un diccionario que será convertido a JSON automáticamente
     return {"Hello": "World"}
 
+# Definimos una ruta para "/items/{item_id}" usando el método HTTP GET
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
+    # Esta función se ejecuta cuando se accede a la ruta /items/{item_id}
+    # item_id es un parámetro de ruta que debe ser un entero
+    # q es un parámetro de consulta opcional de tipo string
+    # Retorna un diccionario con el item_id y el parámetro de consulta q
     return {"item_id": item_id, "q": q}
